5023b9f5bc8b41f7f381b343fabc6e78c405cafb
Fyrngarm <erikthomas146@gmail.com>
2017-10-31 10:14:25 +0000

    Add files via upload
    Core files for adaptation.

---
+++ Blinky - Copy/Blinky/Flash/irq.d
@@ -0,0 +1,7 @@
+.\flash\irq.o: IRQ.c
+.\flash\irq.o: C:\Keil_v5\ARM\Inc\ST\STM32F10x\stm32f10x.h
+.\flash\irq.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm3.h
+.\flash\irq.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\flash\irq.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
+.\flash\irq.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
+.\flash\irq.o: C:\Keil_v5\ARM\Inc\ST\STM32F10x\system_stm32f10x.h
---
+++ Blinky - Copy/Blinky/Flash/retarget.d
@@ -0,0 +1,3 @@
+.\Flash\retarget.o: Retarget.c
+.\Flash\retarget.o: C:\Keil\ARM\RV31\Inc\stdio.h
+.\Flash\retarget.o: C:\Keil\ARM\RV31\Inc\rt_misc.h
