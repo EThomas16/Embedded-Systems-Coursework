b757e15823154ebcd69ae5a0d81059fbd03dbe1c
Fyrngarm <erikthomas146@gmail.com>
2017-11-06 10:18:12 +0000

    Add files via upload

---
+++ Blinky_sound/Flash/serial.d
@@ -0,0 +1,7 @@
+.\Flash\serial.o: Serial.c
+.\Flash\serial.o: C:\Keil\ARM\Inc\ST\STM32F10x\stm32f10x.h
+.\Flash\serial.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+.\Flash\serial.o: C:\Keil\ARM\RV31\Inc\stdint.h
+.\Flash\serial.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\Flash\serial.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\Flash\serial.o: C:\Keil\ARM\Inc\ST\STM32F10x\system_stm32f10x.h
